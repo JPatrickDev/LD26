2acd2609474bf2634c0e0a082ac8d0c64304ccd8
JPatrickDev <jackpatrickmail@gmail.com>
2013-04-27 03:18:46 +0100

    First commit of game code, got window to display, added ideas.txt and rebuilt JEngine.jar for java 1.6

---
+++ src/me/jack/ld26/LD26.java
@@ -0,0 +1,15 @@
+package me.jack.ld26;
+
+import me.jack.JEngine.JEngine;
+
+/**
+ * Author: Jack
+ * Date: 27/04/13
+ */
+public class LD26{
+
+
+    public static void main(String[] args){
+        JEngine.start(new LD26Game("LD26 - Theme: Minimalism"),800,600);
+    }
+}
---
+++ src/me/jack/ld26/LD26Game.java
@@ -0,0 +1,29 @@
+package me.jack.ld26;
+
+import org.newdawn.slick.*;
+
+/**
+ * Author: Jack
+ * Date: 27/04/13
+ */
+public class LD26Game extends BasicGame{
+
+    public LD26Game(String title) {
+        super(title);
+    }
+
+    @Override
+    public void init(GameContainer gameContainer) throws SlickException {
+
+    }
+
+    @Override
+    public void update(GameContainer gameContainer, int i) throws SlickException {
+
+    }
+
+    @Override
+    public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
+
+    }
+}
